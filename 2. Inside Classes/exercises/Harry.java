a9ed9f4eca127b5580685a0f2d4d12899cfa7153
dark knight <Pragnesh@DESKTOP-S18IDVR>
2020-03-14 21:08:55 +0530

    Exercise completed

--- 2. Inside Classes/exercises/Harry.java
+++ 2. Inside Classes/exercises/Harry.java
@@ -27,10 +27,15 @@ public class Harry {
 
 	public static void main(String[] args) {
 		// 1. make harry potter
+		Harry harry = new Harry();
 		// 2. become invisible
+		harry.makeInvisible(true);
 		// 3. spy on professor snape
+		harry.spyOnSnape();
 		// 4. become visible again
-		// 5. cast a “stupefy” spell
+		harry.makeInvisible(false);
+		// 5. cast a spell
+		harry.castSpell("Leviosa");
 	}
 
 }
